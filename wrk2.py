1654063e46578db59596ed3ff5c43fe1b66f43da
unknown <adithyakb144@gmail.com>
2023-09-11 03:34:20 -0700

    file added

---
+++ wrk2.py
@@ -0,0 +1,13 @@
+# l=int(input('enter length of a rectangle'))
+# b=int(input('enter breadth of a rectangle'))
+# area=l*b
+# print("area of a rectangle is",area)
+# perimeter=2*l*b
+# print("perimeter of a rectangle is",perimeter)
+a=int(input('enter a  number'))
+if a>0:
+    print('number is positive')
+elif a<0:
+    print('number is negative')
+else:
+    print('zero')
