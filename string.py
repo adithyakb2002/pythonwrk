181163928d1dc95450100d91ffe832d2a08f48ea
adithya <adithyakb144@gmail.com>
2023-09-18 03:24:32 -0700

    adding

---
+++ string.py
@@ -0,0 +1,14 @@
+# a=str(input('enter your password'))
+# for i in range(1):
+#     print('legth',len(a)) 
+#     print(a.upper())
+#     print(a.isalnum())
+
+# a=str(input('enter your password'))
+# if len(a)>=8:
+#     for i in a:
+#         if i.isdigit():
+
+#         if i.isalnum():
+                 
+  
--- wrk2.py
+++ wrk2.py
@@ -4,10 +4,20 @@
 # print("area of a rectangle is",area)
 # perimeter=2*l*b
 # print("perimeter of a rectangle is",perimeter)
-a=int(input('enter a  number'))
-if a>0:
-    print('number is positive')
-elif a<0:
-    print('number is negative')
+# a=int(input("enter a number"))
+# for i in range(11):
+#     print(a,'x',i,'=',a*i)
+
+# a=int(input("enter a number")) 
+# for i in range(6):
+#     print(i*'*')
+          
+  
+# a=[5,4,3,6,7,3,2,1]
+# for i in a:
+#     print(i*'*')
+a=int(input('enter your birth year'))
+if a>=2000:
+    print("20's kid")
 else:
-    print('zero')
+    print("90's kid")    
