1139ec32742e5af4b03994c70027199210a01201
adithya <adithyakb144@gmail.com>
2023-09-21 03:34:30 -0700

    adding

--- work3.py
+++ work3.py
@@ -1,5 +1,16 @@
-a=input('enter string')
-if a==a[::-1]:
-    print("palindrome")
-else:
-    print("not palindrome")    
+# a=int(input('enter number'))
+# for i in range(1,a+1):
+#     for j in range(1,i):
+#         print(j,end=' ')
+#     for j in range(i,0,-1):
+#         print(j,end=' ')  
+#     print(' ')
+
+a=int(input('enter number'))
+print('*')  
+for i in range(1,a+1):
+    for j in range(1,i):
+        print(j,end=' ')
+    for j in range(i,0,-1):
+        print(j,end=' ') 
+    print('* ')
