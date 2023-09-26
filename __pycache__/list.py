7b874d02ed5d580f8145ef03cd197f21a608920c
adithya <adithyakb144@gmail.com>
2023-09-26 15:55:00 +0530

    adding

---
+++ __pycache__/list.py
@@ -0,0 +1,43 @@
+# my_list=[1,2,3,4,6.0,'adithya',[4,7,9]]
+# print(my_list)
+
+# 2
+# a=[1,2,3,4,5,6,7]
+# b=a[1:4]
+# print(b)
+
+# 3
+# a=[1,2,3]
+# b=[4,5,6]
+# c=a+b
+# print(c)
+
+# 4
+# fruits=['apple','banana','cherry']
+# fruits.append('kiwi')
+# fruits.insert(1,'orange')
+# b=['grape','blueberry']
+# fruits.extend(b)
+# fruits.remove('banana')
+# print(fruits)
+
+# 5
+# a=[5,2,8,1,3]
+# a.sort()
+# print(a)
+# print(a[::-1])
+
+# 8
+# fruits=['apple','banana','cherry']
+# print(fruits.count('apple'))
+
+# 9
+# a=[5,2,8,1,3]
+# print(a[::-1])
+
+# 10
+# original_list=[1,2,3]
+# copied=original_list.copy()
+# copied.append(4)
+# print(copied)
+# print(original_list)
