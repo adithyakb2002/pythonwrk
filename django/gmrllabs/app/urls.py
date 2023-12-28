84bec642e8472fb4789cc0e545ac85225c93678e
adithya <adithyakb144@gmail.com>
2023-12-28 16:32:33 +0530

    adding

--- django/gmrllabs/app/urls.py
+++ django/gmrllabs/app/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
        path('aboutus',views.aboutus,name='aboutus'),
        path('packages',views.packages,name='packages'),
        path('department',views.department,name='department'),
+       path('blog',views.blog,name='blog'),
+       path('gallery',views.gallery,name='gallery'),
+       path('branches',views.branches,name='branches'),
+       path('contactus',views.contactus,name='contactus'),
+       path('bookanappointment',views.bookanappointment,name='bookanappointment'),
+       path('test',views.test,name='test'),
+       path('ayushsilverplan',views.ayushsilverplan,name='ayushsilverplan'),
+
 ]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- django/gmrllabs/app/views.py
+++ django/gmrllabs/app/views.py
@@ -8,5 +8,22 @@ def packages(request):
     return render(request,'packages.html')
 def department(request):
     return render(request,'department.html')
+def blog(request):
+    return render(request,'blog.html')
+def gallery(request):
+    return render(request,'gallery.html')
+def branches(request):
+    return render(request,'branches.html')
+
+def contactus(request):
+    return render(request,'contactus.html')
+def test(request):
+    return render(request,'test.html')
+
+# sub pages
+def bookanappointment(request):
+    return render(request,'bookanappointment.html')
+def ayushsilverplan(request):
+    return render(request,'ayushsilverplan.html')
 
 # Create your views here.
